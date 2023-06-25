d4244d6ab302acea7fb81cb9f5dc1603601028de
Warren <warren.harper@gmail.com>
2023-06-25 13:42:00 -0700

    Chakra setup + initial screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </UserProvider>
+    <ChakraProvider>
+      <UserProvider>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </UserProvider>
+    </ChakraProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,14 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
+import {
+  Button,
+  Flex,
+  Heading,
+  VStack,
+  Image,
+  Box,
+  HStack,
+  Text,
+} from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function Home() {
@@ -23,10 +33,34 @@ export default function Home() {
     );
   } else {
     return (
-      <div>
-        <h2>Shinami in-app wallet demo</h2>
-        <Link href="/api/auth/login">Login</Link>
-      </div>
+      <Flex
+        height="100vh"
+        direction="column"
+        gap="6"
+        align="center"
+        justify="center"
+      >
+        <VStack gap="10" background="#3D3652" p="12" rounded="10">
+          <Heading>In-App Wallet demo</Heading>
+          <Image
+            boxSize="300px"
+            src="https://uploads-ssl.webflow.com/62d16851bc140b39e44135a4/645adbb442ee12e393140809_wallet-hero.svg"
+            alt="Wallet hero"
+          />
+
+          <Button minW="60" colorScheme="teal">
+            <Link href="/api/auth/login">Login</Link>
+          </Button>
+        </VStack>
+        <HStack gap="2">
+          <Text>Powered by</Text>
+          <Image
+            width="100px"
+            src="https://uploads-ssl.webflow.com/62d16851bc140b39e44135a4/63d964a0c586710214df828a_shinami-white.svg"
+            alt="Shinami logo"
+          />
+        </HStack>
+      </Flex>
     );
   }
 }
