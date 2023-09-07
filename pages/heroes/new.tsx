33424bf8749f6e68cfe4e124935b54af58d37edc
Hao Xia <hao@shinami.com>
2023-09-06 17:08:39 -0700

    dummy

--- pages/heroes/new.tsx
+++ pages/heroes/new.tsx
@@ -5,3 +5,9 @@
 //    2b. If non-existing, useNewMintTicket()
 // 3. Allocate attribute points
 // 4. Upon submit, useMintHero() and navigate to /
+
+// Just to please the image builder.
+// Discard this during merge.
+export default function NewPage() {
+  return <></>;
+}
